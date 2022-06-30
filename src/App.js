10b885ebc81e3b8a7aea6c8508065566944d254e
TimD <timothee.diroian@gmail.com>
2022-06-30 10:31:08 +0200

    Hading javascript object inside my JSX code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import * as React from 'react';
 
 function App() {
+  const welcome = {
+    greeting: 'Hey',
+    title: 'React'
+  };
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
     </div>
   );
 }
