c5deb7797adabc0dd16ff9ec8674ae25e1e1d4db
TimD <timothee.diroian@gmail.com>
2022-07-08 10:30:35 +0200

    Passing a props inside the List component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import * as React from 'react';
 
-const dict = [{
-  def1: 'An error in chronology',
-  def2: 'A person or a thing that is chronologically out of place',
-  def3: 'The state or condition of being chronologically out of place', 
-  title: 'Anachronism'},
-  {def1: 'Disposed to seek revenge',
-  def2: 'Intended for or involving revenge',
-  def3: 'The state or condition of being chronologically out of place', 
-  title: 'Vindictive'},
-  {def1: 'An instrument for reproducing sounds by means of the vibration of a stylus or needle following a spiral groove on a revolving disc or cylinder',
-  def2: 'Voyager 1 and 2 took with them golden phonograph records with images and sounds meant to reflect human culture.',
-  def3: 'That was life up until Thomas Edison revolutionized music consumption for the first time in history with the invention of the phonograph, a 10-inch, 78 RPM cylinder disc that could only contain about three minutes of music per side.',  
-  title: 'Phonograph '}];
-
-function List(){
+function List(props){
   return (
     <article>
-      {dict.map( function (word){
-        return (<div>
+      {props.list.map((word) =>{
+        return (<div key={word.id}>
           <h3>{word.title}</h3>
           <ul>
             <li>{word.def1}</li>
@@ -42,6 +28,23 @@ function Search(){
 };
 
 function App() {
+  const dict = [{
+    def1: 'An error in chronology',
+    def2: 'A person or a thing that is chronologically out of place',
+    def3: 'The state or condition of being chronologically out of place', 
+    id: 0,
+    title: 'Anachronism'},
+    {def1: 'Disposed to seek revenge',
+    def2: 'Intended for or involving revenge',
+    def3: 'The state or condition of being chronologically out of place',
+    id: 1,
+    title: 'Vindictive'},
+    {def1: 'An instrument for reproducing sounds by means of the vibration of a stylus or needle following a spiral groove on a revolving disc or cylinder',
+    def2: 'Voyager 1 and 2 took with them golden phonograph records with images and sounds meant to reflect human culture.',
+    def3: 'That was life up until Thomas Edison revolutionized music consumption for the first time in history with the invention of the phonograph, a 10-inch, 78 RPM cylinder disc that could only contain about three minutes of music per side.',  
+    id: 2,
+    title: 'Phonograph '}];
+
   return (
     <div >
       <h1>Welcome to the dictionary</h1>
@@ -49,7 +52,7 @@ function App() {
       
       <Search/>
 
-      <List />
+      <List list={dict} /> {/*We can now create multiple instance of List with diferent object passing in*/}
 
     </div>
   );
